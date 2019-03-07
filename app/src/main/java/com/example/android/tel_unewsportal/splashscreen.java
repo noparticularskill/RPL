b5a0131d3ca166f210ff4a3b161df6f6690d6e6c
ajengrakhmawati <ajengrakhmawati17@gmail.com>
2019-03-07 14:19:31 +0700

    add intent method

--- app/src/main/java/com/example/android/tel_unewsportal/splashscreen.java
+++ app/src/main/java/com/example/android/tel_unewsportal/splashscreen.java
@@ -1,7 +1,10 @@
 package com.example.android.tel_unewsportal;
 
+import android.content.Intent;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class splashscreen extends AppCompatActivity {
 
@@ -9,5 +12,22 @@ public class splashscreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashscreen);
+
+
+        CountDownTimer downTimer = new CountDownTimer(3000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+            }
+
+            @Override
+            public void onFinish() {
+                Intent activity2 = new Intent(splashscreen.this, Splash.class);
+                startActivity(activity2);
+                finish();
+            }
+        };
+        downTimer.start();
     }
 }
+
