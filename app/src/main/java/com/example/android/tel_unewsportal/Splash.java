3a5a89cb58010623b16946aa59049c605386b64b
fianafazriani <fianafazriani@gmail.com>
2019-04-03 20:48:29 +0700

    intent dari login ke main activity

--- app/src/main/java/com/example/android/tel_unewsportal/Splash.java
+++ app/src/main/java/com/example/android/tel_unewsportal/Splash.java
@@ -1,7 +1,9 @@
 package com.example.android.tel_unewsportal;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Splash extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class Splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+    }
+
+    public void register(View view) {
+        Intent hehe = new Intent(Splash.this,Register.class);
+        startActivity(hehe);
     }
 }
