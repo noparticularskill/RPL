5906227efd416a7a7d3c91167e98dc06d3271627
maulanamfathoni <fathoni.toni19@gmail.com>
2019-04-23 17:15:21 +0700

    add News Wall, ngerubah intent news dan menghapus aktivity yang tidak penting

--- app/src/main/java/com/example/android/tel_unewsportal/NewsWall.java
+++ app/src/main/java/com/example/android/tel_unewsportal/NewsWall.java
@@ -3,11 +3,11 @@ package com.example.android.tel_unewsportal;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class TampilanAwalMember extends AppCompatActivity {
+public class NewsWall extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tampilan_awal_member);
+        setContentView(R.layout.activity_news_wall);
     }
 }
